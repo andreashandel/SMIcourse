a733e154ea830cdab56a4838673b6b77309f4b3c
Andreas Handel <andreas.handel@gmail.com>
2020-07-20 09:52:55 -0400

    updates

--- auxiliary/Rcode/Rscripts/iavequations.R
+++
@@ -1,7 +0,0 @@
-###################################################################
-#function specifying the ode model
-#place in extra file to make sure both the fitting and simulation scripts 
-#run the same model
-###################################################################
-
-
